c7c9d26cdc7a5e96975d1d27b41e240e04708d8c
Nukkumattii <aaron.hakala@gmail.com>
2016-10-06 21:11:14 +0300

    changed everything :)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function() {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
 
@@ -10,27 +11,34 @@ function startTimer(duration, display) {
         display.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            timer = duration;
+            timer = 0;
+            document.querySelector('#time').style.visibility = "hidden";
+            document.querySelector('#soaplogo').style.opacity = "1";
         }
     }, 1000);
 }
 
 function getTime() {
-    var fiveMinutes = 60 * 5,
+    var fiveMinutes = 60 * 0.5,
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 };
 
-// BACKGROUND COLOR CHANGE
+
+// CYCLE BETWEEN BACKGROUND COLORS
 
 var colors = ['#FAE03C', '#98DDDE', '#034F84'];
 var active = 0;
-setInterval(function(){
+setInterval(function() {
+
+    // change background color of body
     document.querySelector('body').style.background = colors[active];
+    //change button text hover color
+    $("#myStyle").html('.button:hover {color: '+ colors[active] +'}');
+
     active++;
     if (active == colors.length) active = 0;
-}, 30000);
-
+}, 10000);
 
 
 // OPEN WINDOW
@@ -42,21 +50,21 @@ function openWindow() {
 
     left += window.screenX;
 
-    window.open("kello.html",'windowName','resizable=1,scrollbars=1,fullscreen=0,height=1080,width=' + width + '  , left=' + left + ', toolbar=0, menubar=0,status=1');
+    window.open("kello.html", 'windowName', 'resizable=1,scrollbars=1,fullscreen=0,height=1080,width=' + width + '  , left=' + left + ', toolbar=0, menubar=0,status=1');
     return 0;
 
 }
 
 // Find the right method, call on correct element
 function launchIntoFullscreen(element) {
-  if(element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if(element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if(element.webkitRequestFullscreen) {
-    element.webkitRequestFullscreen();
-  } else if(element.msRequestFullscreen) {
-    element.msRequestFullscreen();
-  }
-  getTime();
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+    getTime();
 }
