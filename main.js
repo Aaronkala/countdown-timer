e64f741ff2f9e7f3806226d15f897b12060185cc
Nukkumattii <aaron.hakala@metropolia.fi>
2016-10-07 09:24:18 +0300

    almost ready

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-function startTimer(duration, display) {
-    var timer = duration,
-        minutes, seconds;
-    setInterval(function() {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+var myTime = {};
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = 0;
-            document.querySelector('#time').style.visibility = "hidden";
-            document.querySelector('#soaplogo').style.opacity = "1";
-        }
-    }, 1000);
-}
-
-function getTime() {
-    var fiveMinutes = 60 * 0.5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
+function time() {
+    var myMinutes = Number(document.getElementById("myMinutes").value);
+    var mySeconds = Number(document.getElementById("mySeconds").value);
+    localStorage.setItem("myMinutes", myMinutes);
+    localStorage.setItem("mySeconds", mySeconds);
+    openWindow();
 };
 
+function getTime() {
+    var duration = localStorage.getItem("duration");
+    var display = localStorage.getItem("display");
+    startTimer(duration, display);
+};
+
+// DISPLAY INPUTTED TIME ON PAGE LOAD
+
+function displayTime() {
+    display = document.querySelector('#time');
+    localStorage.setItem("display", display);
+
+    var myMinutes = localStorage.getItem("myMinutes");
+    var mySeconds = localStorage.getItem("mySeconds");
+    var duration = (60 * Number(myMinutes)) + Number(mySeconds);
+
+    localStorage.setItem("duration", duration);
+
+    var timer = duration,
+        minutes, seconds;
+
+    minutes = parseInt(timer / 60, 10)
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+}
 
 // CYCLE BETWEEN BACKGROUND COLORS
 
@@ -34,7 +47,7 @@ setInterval(function() {
     // change background color of body
     document.querySelector('body').style.background = colors[active];
     //change button text hover color
-    $("#myStyle").html('.button:hover {color: '+ colors[active] +'}');
+    $("#myStyle").html('.button:hover {color: ' + colors[active] + '}');
 
     active++;
     if (active == colors.length) active = 0;
@@ -46,16 +59,17 @@ setInterval(function() {
 function openWindow() {
 
     var width = 1920;
-    var left = 200;
+    var left = 1000;
 
     left += window.screenX;
 
-    window.open("kello.html", 'windowName', 'resizable=1,scrollbars=1,fullscreen=0,height=1080,width=' + width + '  , left=' + left + ', toolbar=0, menubar=0,status=1');
+    window.open("kello.html", 'countdown', 'resizable=1,scrollbars=1,fullscreen=0,height=1080,width=' + width + '  , left=' + left + ', toolbar=0, menubar=0,status=1');
     return 0;
 
 }
 
 // Find the right method, call on correct element
+
 function launchIntoFullscreen(element) {
     if (element.requestFullscreen) {
         element.requestFullscreen();
@@ -68,3 +82,26 @@ function launchIntoFullscreen(element) {
     }
     getTime();
 }
+
+function startTimer(duration, display) {
+    display = document.querySelector('#time');
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function() {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+// THIS IS NOT WORKING! Inputting new values. console.log prints it out.
+
+        display.textContent = minutes + ":" + seconds;
+        console.log(minutes + ":" + seconds);
+        if (--timer < 0) {
+            timer = 0;
+            document.querySelector('#time').style.visibility = "hidden";
+            document.querySelector('#soaplogo').style.opacity = "1";
+        }
+    }, 1000);
+};
