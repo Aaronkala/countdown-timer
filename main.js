2194816e2e3f201c51a26e3473342c2170409606
Nukkumattii <aaron.hakala@metropolia.fi>
2017-01-20 17:47:53 +0200

    fixed

--- main.js
+++ main.js
@@ -39,17 +39,8 @@ function displayTime() {
 }
 
 // SETTING BACKGROUND COLORS
-var colors = [];
-function setColors() {
-    for(i)
-    colors 
-}
+var colors = ['#FAE03C', '#98DDDE', '#034F84'];
 
-// CYCLE BETWEEN BACKGROUND COLORS
-if(colors[0] == null){
-    var colors = ['#FAE03C', '#98DDDE', '#034F84'];
-}
-console.log(colors);
 var active = 0;
 setInterval(function() {
 
